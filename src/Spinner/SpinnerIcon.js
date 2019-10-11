7d7b76cf0f748dae2e00a27ae2e8f1fdb5663a0f
Rickecr <richelton14@gmail.com>
2019-10-10 23:11:02 -0300

    Adicionando o loading ao buscar um pokemon

---
+++ src/Spinner/SpinnerIcon.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const SpinnerIcon = () => {
+    return (
+        <svg
+          width="200px"
+          height="200px"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="xMidYMid"
+          className="lds-rolling"
+          style={{ background: 'none' }}
+        >
+            <circle
+              cx="50"
+              cy="50"
+              fill="none"
+              ng-attr-stroke="{{config.color}}"
+              ng-attr-stroke-width="{{config.width}}"
+              ng-attr-r="{{config.radius}}"
+              ng-attr-stroke-dasharray="{{config.dasharray}}"
+              stroke="#030303"
+              strokeWidth="10"
+              r="35"
+              strokeDasharray="164.93361431346415 56.97787143782138"
+              transform="rotate(256.305 50 50)"
+            >
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  calcMode="linear"
+                  values="0 50 50;360 50 50"
+                  keyTimes="0;1"
+                  dur="1s"
+                  begin="0s"
+                  repeatCount="indefinite"
+                />
+            </circle>
+        </svg>
+    );
+};
+
+export default SpinnerIcon;
